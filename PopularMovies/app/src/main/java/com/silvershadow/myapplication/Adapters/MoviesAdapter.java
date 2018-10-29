5a4cf4edb8515e86f78fe6e69e533fc6b3572fda
KrylovLeonid <lkayakashi@gmail.com>
2018-10-29 21:49:21 +0300

    Revert "Revert "Work done""
    This reverts commit 1a1a14fd3e3fcb0a66a944a24db586550169e5cc.

--- PopularMovies/app/src/main/java/com/silvershadow/myapplication/Adapters/MoviesAdapter.java
+++ PopularMovies/app/src/main/java/com/silvershadow/myapplication/Adapters/MoviesAdapter.java
@@ -1,6 +1,7 @@
 package com.silvershadow.myapplication.Adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -12,6 +13,7 @@ import android.widget.TextView;
 
 import com.silvershadow.myapplication.DataLoading.MovieDataHolder;
 import com.silvershadow.myapplication.Movie;
+import com.silvershadow.myapplication.MovieDetailsActivity;
 import com.silvershadow.myapplication.R;
 import com.silvershadow.myapplication.SupportContract;
 import com.squareup.picasso.Picasso;
@@ -21,18 +23,29 @@ import java.util.List;
 
 public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MoviesViewHolder> {
 
-    private List<Movie> currentMovies = new ArrayList<>();
+
+    public static String POSITION = "position";
+    public static String SORT_TYPE = "sort type";
+
+    public enum SortType{
+        TOP_RATED,
+        POPULAR
+    }
+
+    private static List<Movie> currentMovies;
+    private static SortType currentType;
 
     public MoviesAdapter(){
-        setMoviesToPopular();
+        currentMovies = new ArrayList<>();
+        currentType = SortType.POPULAR;
     }
 
 
-    public class MoviesViewHolder extends RecyclerView.ViewHolder{
+    class MoviesViewHolder extends RecyclerView.ViewHolder{
         ImageView thumbnailIV;
         TextView titleTV;
 
-        public MoviesViewHolder(View itemView) {
+        private MoviesViewHolder(View itemView) {
             super(itemView);
             thumbnailIV = itemView.findViewById(R.id.thumbnail_iv);
             titleTV = itemView.findViewById(R.id.thumbnail_title_tv);
@@ -42,9 +55,23 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MoviesView
 
 
     @Override
-    public void onBindViewHolder(@NonNull MoviesViewHolder holder, int position) {
-        holder.titleTV.setText(currentMovies.get(position).getName());
-        Picasso.get().load(SupportContract.getImgURLstr("w200")).into(holder.thumbnailIV);
+    public void onBindViewHolder(@NonNull final MoviesViewHolder holder, final int position) {
+        holder.titleTV.setText(currentMovies.get(position).getTitle());
+        holder.titleTV.setGravity(TextView.TEXT_ALIGNMENT_GRAVITY);
+        Picasso.get().load(SupportContract.getImgURLstr("w200") + currentMovies.get(position).getThumbImg()).into(holder.thumbnailIV);
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = v.getContext();
+                Intent intent = new Intent(context, MovieDetailsActivity.class);
+                intent.putExtra(POSITION, holder.getAdapterPosition());
+                intent.putExtra(SORT_TYPE,currentType);
+                context.startActivity(intent);
+
+
+            }
+        });
+
     }
 
     @NonNull
@@ -61,15 +88,20 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MoviesView
 
     @Override
     public int getItemCount() {
-        return 0;
+        return currentMovies.size();
+
     }
 
-    public void setMoviesToPopular(){
+    public static void setMoviesToPopular(){
         currentMovies = MovieDataHolder.getPopular();
+        currentType = SortType.POPULAR;
+
+
 
     }
-    public void setMoviesToTopRated(){
+    public static  void setMoviesToTopRated(){
         currentMovies = MovieDataHolder.getTopRated();
+        currentType = SortType.TOP_RATED;
 
     }
 }
--- PopularMovies/app/src/main/java/com/silvershadow/myapplication/MovieDetailsActivity.java
+++ PopularMovies/app/src/main/java/com/silvershadow/myapplication/MovieDetailsActivity.java
@@ -1,6 +1,79 @@
 package com.silvershadow.myapplication;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.silvershadow.myapplication.Adapters.MoviesAdapter;
+import com.silvershadow.myapplication.DataLoading.MovieDataHolder;
+import com.squareup.picasso.Picasso;
 
 public class MovieDetailsActivity extends AppCompatActivity {
+
+    ImageView backgroundIV;
+    ImageView smallIV;
+    TextView titleTV;
+    TextView votesTV;
+    TextView averageRatingTV;
+    TextView releaseDateTV;
+    TextView descriptionTV;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.movie_details_activity);
+        Intent intent = getIntent();
+        int position = intent.getIntExtra(MoviesAdapter.POSITION ,0);
+        MoviesAdapter.SortType type = (MoviesAdapter.SortType) intent.getSerializableExtra(MoviesAdapter.SORT_TYPE);
+
+
+        connectViews();
+        populateViews(position,type);
+
+    }
+
+
+    private void connectViews(){
+         backgroundIV = findViewById(R.id.iv_header_pic);
+         smallIV = findViewById(R.id.iv_movie_pic);
+
+         titleTV = findViewById(R.id.tv_movie_title);
+         votesTV = findViewById(R.id.tv_movie_votes);
+         averageRatingTV = findViewById(R.id.tv_average_rating);
+         releaseDateTV = findViewById(R.id.tv_release_date);
+         descriptionTV = findViewById(R.id.tv_movie_details);
+    }
+
+
+    private void populateViews(int position, MoviesAdapter.SortType sortType){
+        Movie currentMovie = getMovie(position,sortType);
+        Picasso.get().load(SupportContract.getImgURLstr("w400")+ currentMovie.getHeaderImg()).into(backgroundIV);
+        Picasso.get().load(SupportContract.getImgURLstr("w400") + currentMovie.getThumbImg()).into(smallIV);
+        titleTV.setText(currentMovie.getTitle());
+        votesTV.setText(currentMovie.getVotes());
+        averageRatingTV.setText(currentMovie.getAverageRating());
+        releaseDateTV.setText(currentMovie.getReleaseDate());
+        descriptionTV.setText(currentMovie.getDescription());
+        setTitle(currentMovie.getTitle());
+
+    }
+
+    private Movie getMovie(int position, MoviesAdapter.SortType sortType){
+        Movie movie;
+        switch(sortType){
+            case POPULAR:
+                movie = MovieDataHolder.getPopular().get(position);
+                break;
+            case TOP_RATED:
+                movie = MovieDataHolder.getTopRated().get(position);
+                break;
+                default:
+                    movie = null;
+        }
+        return movie;
+    }
 }
